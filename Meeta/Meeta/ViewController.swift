7b2270d12c35c45b209bafcf5990979b21bd0d5f
Shubham Kaliyar <shubhamkaliyar@Shubhams-MacBook-Air.local>
2020-02-09 00:38:55 +0530

    Setup

---
+++ Meeta/Meeta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meeta
+//
+//  Created by Shubham Kaliyar on 09/02/20.
+//  Copyright © 2020 Shubham Kaliyar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
